ed746868c7c433bb40e06c91940dee22acd34d44
Eduard <claygod@yandex.ru>
2017-10-16 11:28:26 +0300

    Add files via upload

--- transaction_bench_test.go
+++ transaction_bench_test.go
@@ -5,6 +5,7 @@ package transaction
 // Copyright © 2016 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
 import (
+	"sync/atomic"
 	"testing"
 )
 
@@ -39,6 +40,7 @@ func BenchmarkFreezeUnfreezeNodeadlock(b *testing.B) {
 	}
 }
 
+/*
 func BenchmarkFreezeUnfreezeDeadlock(b *testing.B) {
 	b.StopTimer()
 	iterat := 1000
@@ -51,6 +53,19 @@ func BenchmarkFreezeUnfreezeDeadlock(b *testing.B) {
 		k.TransactionEnd(uint64(i), uint64(i)+1)
 	}
 }
+*/
+
+func BenchmarkMapRead(b *testing.B) {
+	b.StopTimer()
+	m := make(map[int]bool)
+	for i := 0; i < 70000; i++ {
+		m[i] = true
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		_ = m[int(int16(i))]
+	}
+}
 
 func BenchmarkMapAdd(b *testing.B) {
 	b.StopTimer()
@@ -58,5 +73,50 @@ func BenchmarkMapAdd(b *testing.B) {
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		m[uint64(uint8(i))] = true
+	}
+}
+
+func BenchmarkSliceAdd(b *testing.B) {
+	b.StopTimer()
+	m := make([]bool, 0, 7000000)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		m = append(m, true)
+	}
+}
+
+func BenchmarkCAS(b *testing.B) {
+	b.StopTimer()
+	var m int64 = 0
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		atomic.CompareAndSwapInt64(&m, 0, 0)
+	}
+}
+
+func BenchmarkAtomicStore(b *testing.B) {
+	b.StopTimer()
+	var m int64 = 0
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		atomic.StoreInt64(&m, 0)
+	}
+}
+
+func BenchmarkAtomicLoad(b *testing.B) {
+	b.StopTimer()
+	var m int64 = 0
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		atomic.LoadInt64(&m)
+	}
+}
+
+func BenchmarkAtomicAdd(b *testing.B) {
+	b.StopTimer()
+	var m int64 = 0
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		atomic.AddInt64(&m, 1)
 	}
 }
