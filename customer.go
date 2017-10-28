9a9633c084ab00cacaf66ac314844cba1f187890
Eduard <claygod@yandex.ru>
2017-10-28 16:14:45 +0300

    In `Customer`, `map` is replaced by `sync.Map`

--- customer.go
+++ customer.go
@@ -14,77 +14,33 @@ import (
 
 type Customer struct {
 	m        sync.Mutex
-	accounts map[string]*Account
+	accounts sync.Map
 }
 
 // newCustomer - create new account.
 func newCustomer() *Customer {
-	k := &Customer{accounts: make(map[string]*Account)}
+	k := &Customer{}
 	return k
 }
 
 // Account - get an account link.
 // If there is no such account, it will be created.
 func (c *Customer) Account(num string) *Account {
-	a, ok := c.accounts[num]
-	if !ok {
-		c.m.Lock()
-		a, ok = c.accounts[num]
-		if !ok {
-			a = newAccount(0)
-			c.accounts[num] = a
-		}
-		c.m.Unlock()
-	}
-	return a
-}
-
-func (c *Customer) delAllAccounts(num string) map[string]*Account {
-	aList := make(map[string]*Account)
-	c.m.Lock()
-	for key, a := range c.accounts {
-		if c.delAccountNoLockUnsafe(key) != nil {
-			aList[key] = a
-		} else {
-			delete(c.accounts, key)
-		}
-	}
-	c.m.Unlock()
-	return aList
+	a, _ := c.accounts.LoadOrStore(num, newAccount(0))
+	return a.(*Account)
 }
 
 func (c *Customer) DelAccount(num string) (int64, int64, error) {
-	_, ok := c.accounts[num]
-	if ok {
-		c.m.Lock()
-		defer c.m.Unlock()
-		a, ok := c.accounts[num]
-		if ok {
-			if a.balance == 0 && a.debt == 0 {
-				delete(c.accounts, num)
-				return 0, 0, nil
-			}
-			return a.balance, a.debt, errors.New("Account is not zero.")
-		}
-	}
-	return -1, -1, errors.New("There is no such account")
-}
+	a, ok := c.accounts.Load(num)
 
-func (c *Customer) delAccountNoLockUnsafe(num string) error {
-	a := c.accounts[num]
-	if a.debt == 0 && a.balance == 0 {
-		delete(c.accounts, num)
-		return nil
+	if !ok {
+		return -1, -1, errors.New("There is no such account")
 	}
-	return errors.New("Account is not zero")
-}
-
-/*
-func (c *customer) createAccount(num string, amount int64) error {
-	if _, ok := c.accounts[num]; ok {
-		return errors.New("This account already exists")
+	acc := a.(*Account)
+	b, d := acc.state()
+	if b == 0 && d == 0 {
+		c.accounts.Delete(num)
+		return 0, 0, nil
 	}
-	c.accounts[num] = newAccount(amount)
-	return nil
+	return b, d, errors.New("Account is not zero.")
 }
-*/
