19616e50d6dae3d36a11a30e4ef969db98d2b370
Eduard <claygod@yandex.ru>
2018-01-07 14:00:28 +0300

    Add files via upload

--- storage_test.go
+++ storage_test.go
@@ -1,20 +1,87 @@
 package transactor
 
 // Transactor
-// Storage test
+// Storage/sections test
 // Copyright © 2016 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
-//import "fmt"
 import "testing"
 
-func TestStorage(t *testing.T) {
-	st := newStorage()
-	//fmt.Print(st.data2[st.id(7)])
-	st.addUnit(7)
-	//fmt.Print(st.id(7))
-	//fmt.Print(st.data2[st.id(7)])
-	if st.getUnit(7) == nil {
-		t.Error("Error storage 1")
+func TestStorageNew(t *testing.T) {
+	s := newStorage()
+	if s == nil {
+		t.Error("Error creating `Storage`")
 	}
+	if uint64(len(s.data2)) != storageNumber {
+		t.Error("Error in the number of sections")
+	}
+}
 
+func TestStorageAdd(t *testing.T) {
+	s := newStorage()
+	if !s.addUnit(1) {
+		t.Error("Error adding a unit")
+	}
+	if s.addUnit(1) {
+		t.Error("Failed to add unit again")
+	}
+}
+
+func TestStorageGet(t *testing.T) {
+	s := newStorage()
+	s.addUnit(1)
+	if s.getUnit(1) == nil {
+		t.Error("No unit found (has been added)")
+	}
+	if s.getUnit(2) != nil {
+		t.Error("Found a non-existent unit")
+	}
+}
+
+func TestStorageDel(t *testing.T) {
+	s := newStorage()
+	s.addUnit(1)
+	if _, ok := s.delUnit(1); !ok {
+		t.Error("Unable to delete unit")
+	}
+	if _, ok := s.delUnit(1); ok {
+		t.Error("Repeated deletion of the same unit!")
+	}
+}
+
+func TestSectionNew(t *testing.T) {
+	if newSection() == nil {
+		t.Error("Error creating `Section`")
+	}
+}
+
+func TestSectionAdd(t *testing.T) {
+	s := newSection()
+	if !s.addUnit(1) {
+		t.Error("Error adding a unit")
+	}
+	if s.addUnit(1) {
+		t.Error("Failed to add unit again")
+	}
+}
+
+func TestSectionGet(t *testing.T) {
+	s := newSection()
+	s.addUnit(1)
+	if s.getUnit(1) == nil {
+		t.Error("No unit found (has been added)")
+	}
+	if s.getUnit(2) != nil {
+		t.Error("Found a non-existent unit")
+	}
+}
+
+func TestSectionDel(t *testing.T) {
+	s := newSection()
+	s.addUnit(1)
+	if _, ok := s.delUnit(1); !ok {
+		t.Error("Unable to delete unit")
+	}
+	if _, ok := s.delUnit(1); ok {
+		t.Error("Repeated deletion of the same unit!")
+	}
 }
