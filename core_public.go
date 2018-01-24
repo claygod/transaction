df27a0f1394ad06d5d04d3620833c3bb83259dd1
Eduard <claygod@yandex.ru>
2018-01-24 11:25:04 +0300

    Add files via upload

--- core_public.go
+++ core_public.go
@@ -39,8 +39,8 @@ Two units with the same identifier can not exist.
 
 Returned codes:
 
-	ErrCodeCoreCatch - not obtained permission
-	ErrCodeUnitExist - such a unit already exists
+	ErrCodeCoreCatch // not obtained permission
+	ErrCodeUnitExist // such a unit already exists
 	Ok
 */
 func (c *Core) AddUnit(id int64) errorCodes {
